d1a6397aead3f8f0f4ff90a9c775c971d35b9ef8
khaledsawan <aky2115327@gmail.com>
2026-03-20 20:09:18 +0300

    Inject dependencies

--- lib/di/injection.config.dart
+++ lib/di/injection.config.dart
@@ -19,19 +19,25 @@ import '../core/extension/model_converter_helper.dart' as _i927;
 import '../core/lifecycle/app_lifecycle_service.dart' as _i1073;
 import '../core/network/network_info.dart' as _i6;
 import '../core/network/network_info_impl.dart' as _i927;
-import '../features/carts/data/implements/carts_repository_imp.dart' as _i145;
-import '../features/carts/data/sources/carts_remote_data_source.dart' as _i622;
-import '../features/carts/domain/repositories/carts_repository.dart' as _i434;
-import '../features/carts/domain/usecases/add_cart_use_case/add_cart_use_case.dart'
-    as _i819;
-import '../features/carts/domain/usecases/delete_cart_use_case/delete_cart_use_case.dart'
-    as _i791;
-import '../features/carts/domain/usecases/get_all_carts_use_case/get_all_carts_use_case.dart'
-    as _i211;
-import '../features/carts/domain/usecases/get_cart_by_id_use_case/get_cart_by_id_use_case.dart'
-    as _i692;
-import '../features/carts/domain/usecases/update_cart_use_case/update_cart_use_case.dart'
-    as _i944;
+import '../features/carts/data/implements/cart_local_repository_imp.dart'
+    as _i384;
+import '../features/carts/data/sources/cart_local_data_source.dart' as _i454;
+import '../features/carts/domain/repositories/cart_local_repository.dart'
+    as _i118;
+import '../features/carts/domain/usecases/local_cart/add_product_to_cart_use_case.dart'
+    as _i851;
+import '../features/carts/domain/usecases/local_cart/clear_cart_use_case.dart'
+    as _i395;
+import '../features/carts/domain/usecases/local_cart/decrement_cart_item_use_case.dart'
+    as _i419;
+import '../features/carts/domain/usecases/local_cart/get_cart_items_use_case.dart'
+    as _i759;
+import '../features/carts/domain/usecases/local_cart/get_cart_total_quantity_use_case.dart'
+    as _i203;
+import '../features/carts/domain/usecases/local_cart/increment_cart_item_use_case.dart'
+    as _i684;
+import '../features/carts/domain/usecases/local_cart/remove_cart_item_use_case.dart'
+    as _i656;
 import '../features/products/data/implements/products_repository_imp.dart'
     as _i121;
 import '../features/products/data/sources/products_remote_data_source.dart'
@@ -60,16 +66,40 @@ _i174.GetIt init(
   final apiModule = _$ApiModule();
   gh.singleton<_i1073.AppLifecycleService>(() => _i1073.AppLifecycleService());
   gh.singleton<_i711.Openapi>(() => apiModule.openapi);
+  gh.lazySingleton<_i454.CartLocalDataSource>(
+    () => _i454.CartLocalDataSource(),
+  );
+  gh.singleton<_i6.INetworkInfo>(() => _i927.NetworkInfoImpl.create());
   gh.lazySingleton<_i361.Dio>(() => apiModule.dio(gh<_i711.Openapi>()));
   gh.lazySingleton<_i138.Serializers>(
     () => apiModule.serializers(gh<_i711.Openapi>()),
   );
-  gh.singleton<_i6.INetworkInfo>(() => _i927.NetworkInfoImpl.create());
+  gh.lazySingleton<_i118.CartLocalRepository>(
+    () => _i384.CartLocalRepositoryImp(gh<_i454.CartLocalDataSource>()),
+  );
+  gh.factory<_i851.AddProductToCartUseCase>(
+    () => _i851.AddProductToCartUseCase(gh<_i118.CartLocalRepository>()),
+  );
+  gh.factory<_i395.ClearCartUseCase>(
+    () => _i395.ClearCartUseCase(gh<_i118.CartLocalRepository>()),
+  );
+  gh.factory<_i419.DecrementCartItemUseCase>(
+    () => _i419.DecrementCartItemUseCase(gh<_i118.CartLocalRepository>()),
+  );
+  gh.factory<_i759.GetCartItemsUseCase>(
+    () => _i759.GetCartItemsUseCase(gh<_i118.CartLocalRepository>()),
+  );
+  gh.factory<_i203.GetCartTotalQuantityUseCase>(
+    () => _i203.GetCartTotalQuantityUseCase(gh<_i118.CartLocalRepository>()),
+  );
+  gh.factory<_i684.IncrementCartItemUseCase>(
+    () => _i684.IncrementCartItemUseCase(gh<_i118.CartLocalRepository>()),
+  );
+  gh.factory<_i656.RemoveCartItemUseCase>(
+    () => _i656.RemoveCartItemUseCase(gh<_i118.CartLocalRepository>()),
+  );
   gh.singleton<_i927.ModelConverterHelper>(
     () => _i927.ModelConverterHelper(gh<_i138.Serializers>()),
-  );
-  gh.singleton<_i622.CartsRemoteDataSource>(
-    () => _i622.CartsRemoteDataSource(gh<_i361.Dio>(), gh<_i138.Serializers>()),
   );
   gh.singleton<_i981.ProductsRemoteDataSource>(
     () => _i981.ProductsRemoteDataSource(
@@ -82,27 +112,6 @@ _i174.GetIt init(
       remoteDataSource: gh<_i981.ProductsRemoteDataSource>(),
       networkInfo: gh<_i6.INetworkInfo>(),
     ),
-  );
-  gh.lazySingleton<_i434.CartsRepository>(
-    () => _i145.CartsRepositoryImp(
-      remoteDataSource: gh<_i622.CartsRemoteDataSource>(),
-      networkInfo: gh<_i6.INetworkInfo>(),
-    ),
-  );
-  gh.factory<_i819.AddCartUseCase>(
-    () => _i819.AddCartUseCase(gh<_i434.CartsRepository>()),
-  );
-  gh.factory<_i791.DeleteCartUseCase>(
-    () => _i791.DeleteCartUseCase(gh<_i434.CartsRepository>()),
-  );
-  gh.factory<_i211.GetAllCartsUseCase>(
-    () => _i211.GetAllCartsUseCase(gh<_i434.CartsRepository>()),
-  );
-  gh.factory<_i692.GetCartByIdUseCase>(
-    () => _i692.GetCartByIdUseCase(gh<_i434.CartsRepository>()),
-  );
-  gh.factory<_i944.UpdateCartUseCase>(
-    () => _i944.UpdateCartUseCase(gh<_i434.CartsRepository>()),
   );
   gh.factory<_i45.AddProductUseCase>(
     () => _i45.AddProductUseCase(gh<_i512.ProductsRepository>()),
